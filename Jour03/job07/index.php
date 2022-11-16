ae386669a413f0e9423ff3947546c150bcd74de4
Laura <laura.rodriguez@laplateforme.io>
2022-11-16 15:31:35 +0100

    jobs7 jour3

--- Jour03/job07/index.php
+++ Jour03/job07/index.php
@@ -1,8 +1,20 @@
 <?php
+function mystrlen($str)
+{
+    $count = 0;
+    for ($i = 0; $i < 1000000; $i++) {
+        if (@$str[$i] != "") $count++;
+        else break;
+    }
+    return $count;
+}
 
-$str="Certaines choses changent, et d'autres ne changeront jamais.";
+$str = "Certaines choses changent, et d'autres ne changeront jamais.";
 
-$MYstrlen = NULL;
-
-
-?>
+for ($i = 0; $i < mystrlen($str); $i++) {
+    if ($i == mystrlen($str) - 1) {
+        echo $str[0];
+    } else {
+        echo $str[$i + 1];
+    }
+}
